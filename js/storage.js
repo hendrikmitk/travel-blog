6134680422e0cf7c0deacac46711fe68449aa349
Hendrik H <hharlichs@googlemail.com>
2020-04-14 22:09:14 +0200

    extend storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,29 +1,39 @@
 // Get reference to storage service
 const storage = firebase.storage();
 
-// Create reference file to download
-const fishRef = storage.ref().child("img/fisherman.jpg"); // static image
+// Iterate through Firestore database
+db.collection("posts") // db is defined in main.js
+	.get()
+	.then((posts) => {
+		posts.forEach((post) => {
+			// Get image path from db
+			const imagePath = post.data().image.src;
 
-// Get the download URL
-fishRef
-	.getDownloadURL()
-	.then(function (url) {
-		console.log(url);
-		console.log(fishRef.name);
-	})
-	.catch(function (error) {
-		switch (error.code) {
-			case "storage/object-not-found":
-				// File doesn't exist
-				break;
-			case "storage/unauthorized":
-				// User doesn't have permission to access the object
-				break;
-			case "storage/canceled":
-				// User canceled the upload
-				break;
-			case "storage/unknown":
-				// Unknown error occurred, inspect the server response
-				break;
-		}
+			// Create reference file to download
+			const imgRef = storage.ref().child(`${imagePath}`); // Luckily image src in db matches storage bucket path
+
+			// Get download URL
+			imgRef
+				.getDownloadURL()
+				.then(function (url) {
+					console.log("File name:", imgRef.name);
+					console.log("Storage image url:", url);
+				})
+				.catch(function (error) {
+					switch (error.code) {
+						case "storage/object-not-found":
+							// File doesn't exist
+							break;
+						case "storage/unauthorized":
+							// User doesn't have permission to access the object
+							break;
+						case "storage/canceled":
+							// User canceled the upload
+							break;
+						case "storage/unknown":
+							// Unknown error occurred, inspect the server response
+							break;
+					}
+				});
+		});
 	});
